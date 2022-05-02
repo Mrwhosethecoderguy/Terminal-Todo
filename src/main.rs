dabfddea6ffe9cae0d9c2935d2095235a8fdb723
Mrwhoisthecoderguy <harsh.gumma@gmail.com>
2022-05-02 14:35:14 +0530

    Untick feature added

--- src/main.rs
+++ src/main.rs
@@ -36,10 +36,26 @@ fn main() {
         }
         else if command == "tick" {
             io::stdin().read_line(&mut content).expect("Could not read line");
-            let idx: u32 = content.trim().parse().unwrap();
+            let idx: u32 = content.trim().parse().expect("Enter a number");
+            if idx > todo.len().try_into().unwrap() {
+                println!("Please enter a valid number");
+                continue;
+            }
             let idx: usize = (idx-1).try_into().unwrap();
             done.push(Item {content: todo[idx].content.to_string(), done: true});
             todo.remove(idx);
+        }
+        else if command == "untick" {
+            io::stdin().read_line(&mut content).expect("Could not read line");
+            let idx: u32 = content.trim().parse().expect("Enter a number");
+            if idx > todo.len().try_into().unwrap() {
+                println!("Please enter a valid number");
+                continue;
+            }
+            let idx: usize = (idx-1).try_into().unwrap();
+            todo.push(Item {content: done[idx].content.to_string(), done: false});
+            done.remove(idx);
+
         }
     }
 
